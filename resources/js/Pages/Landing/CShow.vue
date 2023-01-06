d24e8489b4bd75a7410c4f39abb87e027ff1c0b0
fert28 <luisafernanda11917@gmail.com>
2023-01-06 17:16:46 -0600

    .

---
+++ resources/js/Pages/Landing/CShow.vue
@@ -0,0 +1,24 @@
+<script setup>
+import AppHeader from "../../Layouts/AppHeader.vue";
+import CHero from "./CHero.vue";
+import CEjemplo from "./CEjemplo.vue";
+</script>
+<template>
+    <link
+        href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css"
+        rel="stylesheet"
+    />
+    <section id="Welcome" class="bg-zinc-900">
+        <div>
+            <AppHeader />
+            <CHero />
+            <CEjemplo/>
+        </div>
+    </section>
+</template>
+
+<style>
+.relative {
+    background-color: #ebedef;
+}
+</style>
